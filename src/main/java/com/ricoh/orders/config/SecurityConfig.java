d50b1568c78faebbcd1a5cababbf1ba201295ab0
carlos <carlo@DESKTOP-3UN39DE>
2018-04-24 10:58:20 +0200

    Fix variable name error

--- src/main/java/com/ricoh/orders/config/SecurityConfig.java
+++ src/main/java/com/ricoh/orders/config/SecurityConfig.java
@@ -28,11 +28,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Value("${spring.security.admin.password}")
 	private String admin_password;
 
-	@Value("${spring.security.rol_user}")
-	private String rol_user;
+	@Value("${spring.security.role_user}")
+	private String role_user;
 
-	@Value("${spring.security.rol_admin}")
-	private String rol_admin;
+	@Value("${spring.security.role_admin}")
+	private String role_admin;
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -69,9 +69,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser(user).password(user_password).roles(rol_user)
+			.withUser(user).password(user_password).roles(role_user)
 			.and()
-			.withUser(admin).password(admin_password).roles(rol_user, rol_admin);
+			.withUser(admin).password(admin_password).roles(role_user, role_admin);
 	}
     
 }
